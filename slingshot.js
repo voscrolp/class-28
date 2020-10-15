9183e356e3d09c4cd01faf4a7ed975dd9301c92d
voscrolp <70542514+voscrolp@users.noreply.github.com>
2020-10-14 19:58:15 -0500

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,27 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10,
+            damping: 0.1
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+        if(this.sling.bodyA){
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.sling.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+
+}
